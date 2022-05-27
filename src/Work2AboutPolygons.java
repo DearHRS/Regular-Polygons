49021a15e08e1195e579bacb2047ec2b2019a237
DearHRS <78201491+DearHRS@users.noreply.github.com>
2022-05-27 12:51:43 +0500

    Updated main code
    Made few improvements to RegularPolygon class, so updated the main code

--- src/Work2AboutPolygons.java
+++ src/Work2AboutPolygons.java
@@ -17,29 +17,33 @@ public class Work2AboutPolygons {
         Scanner Scan = new Scanner(System.in);
         double[] input = {5, 5};
 
-        /*System.out.println("60 = " + Funx.DegreesToRadians(60));
-        System.out.println("90 = " + Funx.DegreesToRadians(90));
-        System.out.println("120 = " + Funx.DegreesToRadians(120));
-        System.out.println("180 = " + Funx.DegreesToRadians(180));*/
-
-        RegularPolygon Triangle = new RegularPolygon(3, 10, input);
+        RegularPolygon Triangle = new RegularPolygon("Triangle",3, 10, input);
+        System.out.println("\n----------------------------");
         /*System.out.println(" Perimeter = " + Triangle.Perimeter());
         System.out.println(" Area = " + Triangle.Area() + "\n");*/
 
-        RegularPolygon Rectangle = new RegularPolygon(4, 10, input);
+        RegularPolygon Square = new RegularPolygon("Square",4, 10, input);
         /*System.out.println(" Perimeter = " + Rectangle.Perimeter());
         System.out.println(" Area = " + Rectangle.Area() + "\n");*/
+        System.out.println("\n----------------------------");
 
-        RegularPolygon Pentagon = new RegularPolygon(5, 10, input);
+        RegularPolygon Pentagon = new RegularPolygon("Pentagon",5, 10, input);
         /*System.out.println(" Perimeter = " + Pentagon.Perimeter());
         System.out.println(" Area = " + Pentagon.Area() + "\n");*/
+        System.out.println("\n----------------------------");
 
-        RegularPolygon Hexagon = new RegularPolygon(6, 10, input);
+        RegularPolygon Hexagon = new RegularPolygon("Hexagon",6, 10, input);
         /*System.out.println(" Perimeter = " + Hexagon.Perimeter());
         System.out.println(" Area = " + Hexagon.Area() + "\n");*/
+        System.out.println("\n----------------------------");
 
-        RegularPolygon Heptagon = new RegularPolygon(7, 10, input);
+        RegularPolygon Heptagon = new RegularPolygon("Heptagon",7, 10, input);
+        System.out.println("\n----------------------------");
 
-        RegularPolygon Octagon = new RegularPolygon(8, 10, input);
+        RegularPolygon Octagon = new RegularPolygon("Octagon",8, 10, input);
+        System.out.println("\n----------------------------");
+
+        RegularPolygon Nonagon = new RegularPolygon("Nonagon",9, 10, input);
+        System.out.println("\n----------------------------");
     }
 }
